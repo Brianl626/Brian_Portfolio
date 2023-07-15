96919de6eed98d0d0af07c760ad3efda5cfeff25
Brianl626 <104035293+Brianl626@users.noreply.github.com>
2023-07-14 17:45:25 -0700

    Update about.component.ts

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,7 +10,8 @@ export class AboutComponent implements OnInit {
     'Strong Experience in working with Angular using NgRx state management, Angular Material as well as other libraries to create Angular modules, components, directives, and services to deploy Single-Page Application (SPA).',
     'Solid understanding of Angular and familiarity with the rich Angular ecosystem in practical web application development.',
     'Good understanding and hands-on experience of databases like MongoDB, MySQL, and Firebase.',
-    'Experience in creating Java factories for using Spring boot. Implemented Java EE components using Spring MVC, Spring IOC, Spring transactions and Spring MongoDB modules in the development of the Restful web services.',
+    'Experience in creating, building restful APIs with ASP .Net Core.',
+    'Experience testing and debugging Restful ASP.NET Core Web APIs using tools like Fiddler and Postman, coupled with working experience in JavaScript and Angular testing library frameworks including Jasmine and Karma, as well as expertise in debugging tools like Postman.',
     'Strong understanding and experienced with all the phases of agile development methodologies including SCRUM, involved in daily SCRUM meetings to keep track of the Ongoing project status and issues using project tracking software like JIRA, Notion, Trello.'
   ];
 
